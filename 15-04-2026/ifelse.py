11472c16577a7376d1a0d7535365eb3e20c7ca3e
Manjusri S <smanjusri3010@gmail.com>
2026-04-15 14:41:32 +0530

    Add files via upload

---
+++ 15-04-2026/ifelse.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible to vote")
+
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
