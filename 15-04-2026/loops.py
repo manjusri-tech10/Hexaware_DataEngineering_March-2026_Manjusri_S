f0bae2ada38903a661e22ea997febfa983fe09ae
Manjusri S <smanjusri3010@gmail.com>
2026-04-15 15:39:21 +0530

    Add files via upload

---
+++ 15-04-2026/loops.py
@@ -0,0 +1,15 @@
+for i in range(1, 6):
+    print(i)
+
+names = ["Arjun", "Rahul", "Sneha", "Priya"]
+
+for name in names:
+    print(name)
+
+# while
+i = 1
+
+while i <= 5:
+    print(i)
+    i = i + 1
+
