cfff3f8c21320583cc6404904ef2752062fd829e
Manjusri S <smanjusri3010@gmail.com>
2026-05-14 09:37:14 +0530

    Add files via upload

--- 13-05-2026-Project 1/Supply Chain Monitoring and Optimization Platform/Week 5 – Deploy & Automate with Azure DevOps/simulate_pipeline.py
+++ 13-05-2026-Project 1/Supply Chain Monitoring and Optimization Platform/Week 5 – Deploy & Automate with Azure DevOps/simulate_pipeline.py
@@ -1,24 +1,16 @@
 import subprocess
 import datetime
 
-print("=" * 50)
 print("  AZURE DEVOPS PIPELINE SIMULATION")
 print(f"  Started at: {datetime.datetime.now()}")
-print("=" * 50)
-
-# Step 1
 print("\n[STEP 1] Set up Python - Done")
 
-# Step 2
 print("\n[STEP 2] Installing Dependencies...")
 subprocess.run(["pip", "install", "-r", "requirements.txt"])
 print("Dependencies installed successfully.")
 
-# Step 3
 print("\n[STEP 3] Running Supply Chain Script...")
 subprocess.run(["python", "run_pipeline.py"])
 
-# Step 4
 print("\n[STEP 4] Log Completion")
 print(f"Pipeline completed successfully at: {datetime.datetime.now()}")
-print("=" * 50)
